0049a8583fe1191a8f6b29dba7f8eeb50280ddb2
Alan Brookhart <alan@brookhart.org>
2023-03-01 10:15:38 -0500

    Update read_data.R
    changing prevalence of PUD outcome

--- data-raw/read_data.R
+++ data-raw/read_data.R
@@ -95,7 +95,7 @@ ns = ns %>% select(-starts_with("reas1")) %>%
 
 # simulate outcome
 
-pbleed = 1/(1+exp(-(-8 + .6*I(ns$anti_coagulant_use == "Yes") + .5*I(ns$corticosteroid_use == "Yes") +
+pbleed = 1/(1+exp(-(-6.5 + .6*I(ns$anti_coagulant_use == "Yes") + .5*I(ns$corticosteroid_use == "Yes") +
                     .2 * I(ns$aspirin_use == "Yes") + .7*I(ns$arthritis == "Yes") + .07*ns$age + .3*I(ns$sex == "Male") +
                     .02*I(ns$race == "Black") - .3 * I(ns$cox2_initiation =="Yes" ))))
 
